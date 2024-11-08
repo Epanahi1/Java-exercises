dfeebb59f0d787ee19fd7e898860090b384c0f8b
Epanahi1 <a.a.panahi1@gmail.com>
2024-11-08 21:56:00 +0330

    Create grade.java

---
+++ grade.java
@@ -0,0 +1,23 @@
+package ir.ics.traning;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		int Score=10;
+	    switch(Score){
+		
+		case 1,2,3,4,5: 
+			System.out.println("garade is D");
+		break;
+		case 6,7,8,9,10: 
+			System.out.println("garade is C");
+		break;	
+		case 11,12,13,14,15: 
+			System.out.println("garade is B");
+		break;
+		case 16,17,18,19,20: 
+			System.out.println("garade is A");
+		break;
+	}
+}
+}
